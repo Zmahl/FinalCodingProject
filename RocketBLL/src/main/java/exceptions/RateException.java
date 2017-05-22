847c1776a1fb44782402ccfed7018dcec9f14ffc
Zach <Zach@128.175.187.123>
2017-05-21 21:13:39 -0400

    Done with these

--- RocketBLL/src/main/java/exceptions/RateException.java
+++ RocketBLL/src/main/java/exceptions/RateException.java
@@ -1,9 +1,18 @@
 package exceptions;
 
+import rocketDomain.RateDomainModel;
+
 public class RateException extends Exception {
 
-	//	TODO - RocketBLL RateException - RateDomainModel should be an attribute of RateException
-	//	* Add RateRomainModel as an attribute
-	//	* Create a constructor, passing in RateDomainModel
-	//	* Create a getter (no setter, set value only in Constructor)
+	private RateDomainModel rdm;
+
+	public void RateException(RateDomainModel rdm){
+		rdm = this.rdm;
+	
+	}
+
+	public RateDomainModel getRateDomainModel(){
+		return rdm;
+	}
+
 }
--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -4,15 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import exceptions.RateException;
+import java.util.ArrayList;
+
+import rocketDomain.RateDomainModel;
 
 public class rate_test {
 
-	//TODO - RocketBLL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketBLL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
-
+	@Test
+	public void Rate_test() {
+		ArrayList <RateDomainModel> allRates= RateDAL.getAllRates();
+			System.out.println("Rate's size: "+ allRates.size());
+		assert(allRates.size()>0);
+		
+		assertEquals(allRates.get(0).getdInterestRate(), 1.00, 0.03);
+		assertEquals(allRates.get(1).getdInterestRate(), 1.50,  0.03);
+		assertEquals(allRates.get(2).getdInterestRate(), 2.00,  0.03);
+		assertEquals(allRates.get(3).getdInterestRate(), 2.50, 0.03);
+		assertEquals(allRates.get(4).getdInterestRate(), 3.00,  0.03);	
+		
+	}
+	@Test
+	public void test() {
+		
+		ArrayList <RateDomainModel> rate= RateDAL.getAllRates();
+			System.out.println("Rate's size: "+ rate.size());
+		assert(rate.size()>0);
+	}
 }
